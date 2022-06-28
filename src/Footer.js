e9ace2d97ec9d8dd71ff91e28149ae563af48b3f
Chris Bailey <chris85bailey@aol.co.uk>
2022-06-28 17:13:50 +0100

    changed div on the footer to a footer tag

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-    <div className="attribution">
+    <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="https://twitter.com/bailey_coding" target="_blank" rel="noreferrer">Chris Bailey</a>.
-    </div>  
+    </footer>  
   );
 }
  
